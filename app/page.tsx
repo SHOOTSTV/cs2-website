1cc26decda4e802e82fb49a30545cfff9d79bf69
Axel <72924802+SHOOTSTV@users.noreply.github.com>
2025-03-03 21:16:22 +0100

    feat: added Weapons section using 3d models with React Three Fiber

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { About } from "@/components/About";
 import { Hero } from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import { Weapons } from "@/components/Weapons";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Weapons />
     </main>
   );
 }
