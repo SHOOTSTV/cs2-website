5d34156de15e2c2e110f86d86b52231bf002dcf4
Axel <72924802+SHOOTSTV@users.noreply.github.com>
2025-03-08 15:06:29 +0100

    feat: responsive for weapons tab

--- components/WeaponModel.tsx
+++ components/WeaponModel.tsx
@@ -1,6 +1,5 @@
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import React from "react";
 
 function WeaponModel({
   modelName,
@@ -13,31 +12,33 @@ function WeaponModel({
   model.scene.position.set(0, modelName === "AK-47" ? 0 : -0.5, -0.5);
 
   return (
-    <Canvas
-      className="cursor-pointer"
-      frameloop="demand"
-      camera={{
-        position: [0, 0, 7],
-        fov: 25,
-        near: 0.1,
-      }}
-    >
-      <OrbitControls
-        autoRotate
-        autoRotateSpeed={2}
-        enableZoom={false}
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-      />
-      <ambientLight intensity={2} />
-      <directionalLight position={[5, 5, 5]} intensity={1.5} />
-      <directionalLight position={[-5, -5, -5]} intensity={0.5} />
-      <primitive
-        object={model.scene}
-        scale={3}
-        rotation={[0, Math.PI / 4, 0]}
-      />
-    </Canvas>
+    <div className="w-full h-[300px] md:h-[500px]">
+      <Canvas
+        className="cursor-pointer touch-none"
+        frameloop="demand"
+        camera={{
+          position: [0, 0, 7],
+          fov: 25,
+          near: 0.1,
+        }}
+      >
+        <OrbitControls
+          autoRotate
+          autoRotateSpeed={2}
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <ambientLight intensity={2} />
+        <directionalLight position={[5, 5, 5]} intensity={1.5} />
+        <directionalLight position={[-5, -5, -5]} intensity={0.5} />
+        <primitive
+          object={model.scene}
+          scale={3}
+          rotation={[0, Math.PI / 4, 0]}
+        />
+      </Canvas>
+    </div>
   );
 }
 
